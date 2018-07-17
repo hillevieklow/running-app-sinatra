9f44b604dcce33499e4d75031ae09b5c00e5e33e
Hillevi Eklow <hillevieklow@gmail.com>
2018-07-17 12:03:19 -0400

    enables get/posts requests and redirects to the right .erb files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,34 @@
 class UsersController < ApplicationController
+  get '/signup' do
+    erb :'users/signup'
+  end
+
+  post '/signup' do
+   if params[:username] == "" || params[:email] == "" || params[:password] == ""
+     erb :'users/signup'
+    else
+     @user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+     session[:user_id] = @user.id
+     redirect '/runs/home'
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect '/runs/home'
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+    if user && user.authenticate(params[:password])
+    session[:user_id] = user.id
+    redirect "/runs/home"
+  else
+    erb :'users/signup'
+   end
+ end
+
 end
